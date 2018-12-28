d978c66a7b85fc63afa07531871ab704e76ecb48
Ayan Sengupta <ayan.sengupta27@gmail.com>
2018-12-28 18:35:40 -0500

    final basic setup

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Recipe } from "../recipe.model";
 
 @Component({
-  selector: 'app-recipe-list',
-  templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  selector: "app-recipe-list",
+  templateUrl: "./recipe-list.component.html",
+  styleUrls: ["./recipe-list.component.css"]
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      "RibEye Steak",
+      "A delectable medium rare steak",
+      "https://www.omahasteaks.com/gifs/990x594/pr_ts004.jpg"
+    ),
+    new Recipe(
+      "Tiramisu",
+      "A classic italian desert",
+      "https://images-gmi-pmc.edge-generalmills.com/2dc58c2e-3d2d-4797-8866-a21964b55921.jpg"
+    )
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
